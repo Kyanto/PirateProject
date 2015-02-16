74e62913c46812acd1874a93406b930b4c75cac8
Kyanto <tiiko.kyanto@hotmail.fr>
2015-02-16 21:43:55 +0100

    Update and rename interface.h to interface.hpp

--- interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACE_H
-#define INTERFACE_H
-
-#include <SFML/Graphics.hpp>
-#include "engine.h"
-
-void game() ;
-
-#endif // __INTERFACE_H__
---
+++ interface.hpp
@@ -0,0 +1,8 @@
+#ifndef INTERFACE_HPP
+#define INTERFACE_HPP
+
+#include <SFML/Graphics.hpp>
+
+void game() ;
+
+#endif // INTERFACE_HPP
